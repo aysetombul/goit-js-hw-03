eef23a6e4c443c95877b8a597f72ea692cc645be
aysetombul <agamzetombul@gmail.com>
2025-01-02 19:53:34 +0300

    düzeltme

--- js/task-2.js
+++ js/task-2.js
@@ -2,3 +2,10 @@ function makeArray(firstArray, secondArray, maxLength) {
     const newArray = firstArray.concat(secondArray); // Combine the arrays
     return newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray; // Trim if necessary
 }
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
